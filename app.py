fc8efcf06d0abb959f48a91bd0bcc05d35c54da1
David Rm <155977317+drodmay1@users.noreply.github.com>
2024-03-11 18:11:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,24 +9,31 @@ from shiny import reactive, render, req
 # Use the built-in function to load the Palmer Penguins dataset
 penguins_df = palmerpenguins.load_penguins()
 
+# names the page
 ui.page_opts(title="DavidRm Penguin", fillable=True)
 
+# creates sidebar for user interaction
 with ui.sidebar(open="open"):
     
     ui.h2("Sidebar")
     
+    # Creates a dropdown input to choose a column 
     ui.input_selectize(
         "selected_attribute",
         "Select Plotly Attribute",
         ["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g"],
     )
 
+    # Creates a numeric input for the number of Plotly histogram bins
     ui.input_numeric("plotly_bin_count", "Number of Plotly bins", 30)
     
+    # Creates a slider input for the number of Seaborn bins
     ui.input_slider("seaborn_bin_count", "Number of Seaborn bins", 1, 100, 20)
 
+    # Adds a horizontal rule to the sidebar
     ui.hr()
     
+    # Creates a checkbox group input
     ui.input_checkbox_group(
         "selected_species_list",
         "Species",
@@ -35,6 +42,7 @@ with ui.sidebar(open="open"):
         inline=True,
     )
 
+    # Adds a hyperlink to GitHub Repo
     ui.a(
         "GitHub",
          href="https://github.com/drodmay1/cintel-02-data.git",
@@ -51,7 +59,7 @@ with ui.layout_columns(col_widths=(4, 8)):
 
     @render.data_frame
     def render_penguins_table():
-        return penguins_df
+        return filtered_data()
 
 # Creates a DataGrid showing all data
 
@@ -61,15 +69,10 @@ with ui.layout_columns(col_widths=(4, 8)):
 
     ui.h2("Penguins DataGrid")
 
-@render.ui()
-def rows():
-    rows = input.penguins_df_selected_rows()  
-    selected = ", ".join(str(i) for i in sorted(rows)) if rows else "None"
-    return f"Rows selected: {selected}"
 
 @render.data_frame
 def penguins_data():
-    return render.DataGrid(penguins_df, row_selection_mode="multiple") 
+    return render.DataGrid(filtered_data(), row_selection_mode="multiple") 
 
 # Creates a Plotly Histogram showing all species
 
@@ -79,7 +82,7 @@ with ui.card(full_screen=True):
     @render_plotly
     def plotly_histogram():
         return px.histogram(
-            penguins_df, x=input.selected_attribute(), nbins=input.plotly_bin_count()
+            filtered_data(), x=input.selected_attribute(), nbins=input.plotly_bin_count()
         )
 
 # Creates a Seaborn Histogram showing all species
@@ -89,7 +92,7 @@ with ui.card(full_screen=True):
 
     @render.plot(alt="Seaborn Histogram")
     def seaborn_histogram():
-        histplot = sns.histplot(data=penguins_df, x="body_mass_g", bins=input.seaborn_bin_count())
+        histplot = sns.histplot(filtered_data(), x="body_mass_g", bins=input.seaborn_bin_count())
         histplot.set_title("Palmer Penguins")
         histplot.set_xlabel("Mass")
         histplot.set_ylabel("Count")
@@ -102,7 +105,7 @@ with ui.card(full_screen=True):
 
     @render_plotly
     def plotly_scatterplot():
-        return px.scatter(penguins_df,
+        return px.scatter(filtered_data(),
             x="bill_length_mm",
             y="body_mass_g",
             color="species",
@@ -113,3 +116,16 @@ with ui.card(full_screen=True):
             },
             size_max=8, 
         )
+
+# --------------------------------------------------------
+# Reactive calculations and effects
+# --------------------------------------------------------
+
+# Add a reactive calculation to filter the data
+# By decorating the function with @reactive, we can use the function to filter the data
+# The function will be called whenever an input functions used to generate that output changes.
+# Any output that depends on the reactive function (e.g., filtered_data()) will be updated when the data changes.
+
+@reactive.calc
+def filtered_data():
+    return penguins_df
